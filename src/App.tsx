213864a00a5a0c9b83144cc2c07b74f509b8bec2
Shavindi Santhupama Liyanage <shavis1113@gmail.com>
2025-11-13 14:12:15 +0530

    Implement the folder structure (#5)
    Co-authored-by: ShavindiLiyanage <ssanthupamaliyanage@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-// src/App.tsx
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import LandingPage from './pages/landingPage.tsx';
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/resume" element={<Resume />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blog" element={<Blog />} /> */}
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </div>
   );
