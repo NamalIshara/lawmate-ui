f20f3305de14d774f83b94d376604fdd61d44ddb
Shavindi Santhupama Liyanage <shavis1113@gmail.com>
2025-11-23 02:17:28 +0530

    Commit (#15)
    * Implement the folder structure
    
    * Footer Implementation
    
    * Make the order of the pages
    
    * Link the pages
    
    * commit
    
    ---------
    
    Co-authored-by: ShavindiLiyanage <ssanthupamaliyanage@gmail.com>

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import Header from '../components/main/header.tsx';
 import Hero from '../components/main/hero.tsx';
 import Features from '../components/main/features.tsx';
-// import HowItWorks from '../components/main/howItWorks.tsx';
-// import Features from '../components/main/features.tsx';
 import HowItWorks from '../components/main/howItWorks.tsx';
-// import AboutUs from '../components/main/aboutUs.tsx';
-// import HowItWorks from '../components/main/howItWorks.tsx';
 import ContactUs from '../components/main/contactUs.tsx';
 import Footer from '../components/main/footer.tsx';
-import '../styles/global.css';
 import AboutUs from '../components/main/aboutUs.tsx';
 
 const Landing: React.FC = () => {
@@ -18,11 +13,7 @@ const Landing: React.FC = () => {
         <Header />
         <Hero />
         <Features />
-        {/*<HowItWorks />*/}
-        {/*<AboutUs />*/}
-        {/*<ContactUs />*/}
-
-        {<HowItWorks />}
+        <HowItWorks />
         <AboutUs />
         <ContactUs />
         <Footer />
